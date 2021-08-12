0d4c8d629390830c1c8c77c80be6da39531f87dc
maxolee23 <maxolee23@gmail.com>
2021-08-12 15:07:15 -0500

    delete functionality works

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -3,16 +3,20 @@ import axios from 'axios';
 
 const Post = ({post, getPosts}) => {
 
-    async function deletePost(){
-        const deleted = await axios.delete(`http://localhost:5000/posts/:${post._id}`);
-        console.log(deleted)
-        // await getPosts();
+    // async function deletePost(post){
+    //     // const deleted = await axios.delete(`http://localhost:5000/posts/${post._id}`);
+    //     console.log(post)
+    //     // await getPosts();
         
+    // }
+    const deletePost = async () => {
+        // console.log(post._id)
+        await axios.delete(`http://localhost:5000/posts/${post._id}`);
+        await getPosts();
     }
     return (
         <div>
-            <h1>{post.title}</h1>
-            <h2>{post._id}</h2>
+            <p>{post.title}</p>
             <button onClick={deletePost}>Delete</button>
         </div>
     )
